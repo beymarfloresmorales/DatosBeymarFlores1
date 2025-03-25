faa6d1b9a4d6c1e8ba5c549068ba1d920a3f385c
Beymar Flores Morales <bfm764@educa.madrid.org>
2025-03-25 20:52:06 +0100

    version inicial

---
+++ DatosBeymarflores/src/datosbeymarflores/datosbeymarflores.java
@@ -0,0 +1,10 @@
+package datosbeymarflores;
+
+public class datosbeymarflores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("soy beymar flores");
+	}
+
+}
